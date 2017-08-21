b2e4c588f6252766bab699f11ae999472683f0cf
timetam1 <tanuma321bre@gmail.com>
2017-08-21 12:41:37 +0900

    Create contorol3.php

---
+++ contorol3.php
@@ -0,0 +1,24 @@
+# CRUDsystem-MVC-
+<?php
+class ind {
+	public function in(){
+
+?>
+
+<table>
+	<tr>
+		<th>id</th>
+		<th>名前</th>
+		<th>価格</th>
+		<th>キーワード</th>
+
+	</tr>
+	<tr>
+		
+
+	</tr>
+</table>
+<?php
+}
+}
+?>
