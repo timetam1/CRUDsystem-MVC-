195b944a867858c0afface24f24a1c224c6639d7
timetam1 <tanuma321bre@gmail.com>
2017-08-21 12:44:23 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+# CRUDsystem-MVC-
+
+<p>お選びください</p>
+<form action = "allcontorol.php" method ="post">
+	<input type = "radio" name ="1" value = "on">
+	<label>入力</label>
+	<input type = "radio" name ="2" value   ="on">
+	<label>編集</label>
+	<input type = "radio" name ="3" value = "on">
+	<label>削除</label>
+	<input type ="radio" name = "4" value  = "on">
+	<label>一覧画面</label>
+	<input type  = "submit" value = "送信する">
+
+</form>
