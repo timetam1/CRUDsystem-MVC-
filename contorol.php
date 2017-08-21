3e3f23f43db720f2b57221eaacbf7f7e122aacb2
timetam1 <tanuma321bre@gmail.com>
2017-08-21 12:40:28 +0900

    Create contorol.php

---
+++ contorol.php
@@ -0,0 +1,37 @@
+# CRUDsystem-MVC-
+<?php
+class cot{
+	public function co(){
+
+	
+
+?>
+<form action = "done.php" method = "post">
+<dl>
+	<dt>id</dt>
+	<dd>
+		<input type = "text" name ="id">
+	</dd>
+	<dt>商品名</dt>
+	<dd>
+		<input type  = "text" name  ="name">
+	</dd>
+	<dt>価格</dt>
+	<dd>
+		<input type  = "text" name ="price">
+	</dd>
+	<dt>キーワード</dt>
+	<dd>
+		<input type  = "text" name = "keyword">
+	</dd>
+	<input type   ="submit" value = "送信する">
+
+
+</dl>
+	
+</form>
+
+<?php
+}
+}
+?>
